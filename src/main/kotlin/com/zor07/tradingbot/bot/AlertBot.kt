6bfca28c28ef2abc341732e840bb6231673cc711
Anzor Karmov <a.karmov@lipt-soft.ru>
2026-09-10 15:02:03 +0300

    Fix Telegram proxy: use DefaultBotOptions with SOCKS5 instead of system properties

--- src/main/kotlin/com/zor07/tradingbot/bot/AlertBot.kt
+++ src/main/kotlin/com/zor07/tradingbot/bot/AlertBot.kt
@@ -4,6 +4,7 @@ import com.zor07.tradingbot.config.properties.AppProperties
 import com.zor07.tradingbot.config.properties.TelegramProperties
 import com.zor07.tradingbot.user.UserService
 import org.springframework.stereotype.Component
+import org.telegram.telegrambots.bots.DefaultBotOptions
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
@@ -13,7 +14,19 @@ class AlertBot(
     properties: TelegramProperties,
     private val appProperties: AppProperties,
     private val userService: UserService
-) : TelegramLongPollingBot(properties.botToken) {
+) : TelegramLongPollingBot(buildOptions(properties), properties.botToken) {
+
+    companion object {
+        private fun buildOptions(properties: TelegramProperties): DefaultBotOptions {
+            val options = DefaultBotOptions()
+            if (properties.proxyHost.isNotBlank() && properties.proxyPort > 0) {
+                options.proxyHost = properties.proxyHost
+                options.proxyPort = properties.proxyPort
+                options.proxyType = DefaultBotOptions.ProxyType.SOCKS5
+            }
+            return options
+        }
+    }
 
     private val username = properties.botUsername
 
--- src/main/kotlin/com/zor07/tradingbot/config/TelegramBotConfig.kt
+++ src/main/kotlin/com/zor07/tradingbot/config/TelegramBotConfig.kt
@@ -1,27 +1,16 @@
 package com.zor07.tradingbot.config
 
 import com.zor07.tradingbot.bot.AlertBot
-import com.zor07.tradingbot.config.properties.TelegramProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.telegram.telegrambots.meta.TelegramBotsApi
-import org.telegram.telegrambots.meta.generics.BotSession
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
 @Configuration
-class TelegramBotConfig(
-    private val alertBot: AlertBot,
-    private val telegramProperties: TelegramProperties
-) {
+class TelegramBotConfig(private val alertBot: AlertBot) {
 
     @Bean
     fun telegramBotsApi(): TelegramBotsApi {
-        if (telegramProperties.proxyHost.isNotBlank() && telegramProperties.proxyPort > 0) {
-            System.setProperty("https.proxyHost", telegramProperties.proxyHost)
-            System.setProperty("https.proxyPort", telegramProperties.proxyPort.toString())
-            System.setProperty("socksProxyHost", telegramProperties.proxyHost)
-            System.setProperty("socksProxyPort", telegramProperties.proxyPort.toString())
-        }
         val api = TelegramBotsApi(DefaultBotSession::class.java)
         api.registerBot(alertBot)
         return api
