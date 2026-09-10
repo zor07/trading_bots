41f2bbbf8afcf363e2f7271ef58445747e79a451
Anzor Karmov <a.karmov@lipt-soft.ru>
2026-09-10 14:40:51 +0300

    Add SOCKS5 proxy support for Telegram bot

--- src/main/kotlin/com/zor07/tradingbot/config/properties/TelegramProperties.kt
+++ src/main/kotlin/com/zor07/tradingbot/config/properties/TelegramProperties.kt
@@ -5,5 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "telegram")
 data class TelegramProperties(
     val botToken: String,
-    val botUsername: String
+    val botUsername: String,
+    val proxyHost: String = "",
+    val proxyPort: Int = 0
 )
